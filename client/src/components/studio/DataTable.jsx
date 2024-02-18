934357e30a41b4984ad033cf6e52b0b99cff32fc
Dev Code <hellodev_code@outlook.com>
2024-02-18 21:14:39 +0530

    created-n-used DataTable component

---
+++ client/src/components/studio/DataTable.jsx
@@ -0,0 +1,67 @@
+import React from "react";
+import { DataGrid } from "@mui/x-data-grid";
+import NoContent from "./NoContent";
+
+const DataTable = ({ rowsData, theme, columns }) => {
+  return (
+    <>
+      <DataGrid
+        rows={rowsData.length !== 0 ? rowsData : []}
+        columns={columns}
+        initialState={{
+          pagination: {
+            paginationModel: { page: 0, pageSize: 30 },
+          },
+        }}
+        pageSizeOptions={[30, 50, 100]}
+        checkboxSelection
+        sx={{
+          borderLeft: "none",
+          borderRight: "none",
+          "& .MuiDataGrid-virtualScroller": {
+            height: rowsData.length === 0 ? "55px" : "auto",
+          },
+          "& .MuiDataGrid-virtualScrollerContent": {
+            height: rowsData.length === 0 ? "auto" : "calc(100vh - 300px) !important",
+          },
+          "& .MuiDataGrid-cell": {
+            maxHeight: "100% !important",
+            ":focus, :focus-within": {
+              outline: "none",
+            },
+          },
+          "& .MuiDataGrid-row": {
+            maxHeight: "100% !important",
+            padding: "8px 0px 8px 32px",
+            margin: "10px 0",
+            borderBottom: "1px solid #515151 !important",
+            ":hover": {
+              backgroundColor: theme.palette.studioDarkGray,
+            },
+          },
+          "& .MuiDataGrid-row.Mui-selected": {
+            backgroundColor: `${theme.palette.studioDarkGray} !important`,
+            ":hover": {
+              backgroundColor: `${theme.palette.studioDarkGray} !important`,
+            },
+          },
+          "& .MuiDataGrid-columnHeaders": {
+            borderBottom: "1px solid #515151 !important",
+            paddingLeft: "32px",
+          },
+          "& .MuiDataGrid-columnHeader": {
+            ":focus,:focus-within": {
+              outline: "none",
+            },
+          },
+          "& .MuiDataGrid-withBorderColor": {
+            border: "none",
+          },
+        }}
+      />
+      {rowsData.length === 0 ? <NoContent /> : null}
+    </>
+  );
+};
+
+export default DataTable;
--- client/src/scenes/studio/index.jsx
+++ client/src/scenes/studio/index.jsx
@@ -1,13 +1,14 @@
 import React, { useEffect, useState } from "react";
-import { DataGrid } from "@mui/x-data-grid";
 import { useTheme } from "@emotion/react";
 import { useGetMySlimVideosQuery, useGetMyVideosQuery } from "../../state/api";
 import { useSelector } from "react-redux";
 import { decodeToken } from "react-jwt";
 import { Box, Typography } from "@mui/material";
-import NoContent from "../../components/studio/NoContent";
+
 import SelectContentNavbar from "../../components/studio/SelectContentNavbar";
 import StudioVideoCardComponent from "../../components/studio/StudioVideoCardComponent";
+import DataTable from "../../components/studio/DataTable";
+
 import { transformArray } from "../../components/FormatFns";
 
 const Studio = () => {
@@ -44,6 +45,7 @@ const Studio = () => {
   const [selected, setSelected] = useState("Videos");
   const token = useSelector((state) => state.global.token);
   const decodedToken = !token ? null : decodeToken(token);
+
   const { data: videoData, isLoading: vidLoading } = useGetMyVideosQuery({ userId: decodedToken.id, page: 1, pageSize: 30 });
   const { data: shortVideoData, isLoading: shortVidLoading } = useGetMySlimVideosQuery({ userId: decodedToken.id, page: 1, pageSize: 30 });
 
@@ -69,108 +71,15 @@ const Studio = () => {
   }, [shortVidLoading, shortVideoData]);
 
   // Create a list, in which, when a list-item is selected custom data-grid is rendered depending upon
-  // the list-item-state 
+  // the list-item-state
   return (
     <div>
       <Box sx={{ paddingLeft: "32px", paddingTop: "23px", position: "sticky", left: "0", marginBottom: "15px" }}>
         <Typography variant="h3">Channel content</Typography>
       </Box>
       <SelectContentNavbar theme={theme} setSelected={setSelected} selected={selected} />
-      {selected === "Videos" ? (
-        <>
-          {" "}
-          <DataGrid
-            rows={videoRowsData.length !== 0 ? videoRowsData : []}
-            columns={columns}
-            initialState={{
-              pagination: {
-                paginationModel: { page: 0, pageSize: 30 },
-              },
-            }}
-            pageSizeOptions={[30, 50, 100]}
-            checkboxSelection
-            sx={{
-              // paddingLeft: "32px",
-              borderLeft: "none",
-              borderRight: "none",
-              "& .MuiDataGrid-virtualScroller": {
-                height: videoRowsData.length === 0 ? "55px" : "auto",
-                // paddingLeft:"32px"
-              },
-              "& .MuiDataGrid-virtualScrollerContent": {
-                height: videoRowsData.length === 0 ? "auto" : "calc(100vh - 300px) !important",
-              },
-              "& .MuiDataGrid-cell": {
-                maxHeight: "100% !important",
-                ":focus": {
-                  outline: "none",
-                },
-              },
-              "& .MuiDataGrid-row": {
-                maxHeight: "100% !important",
-                padding: "8px 0px 8px 32px",
-                margin: "10px 0",
-                borderBottom: "1px solid #515151 !important",
-              },
-              "& .MuiDataGrid-columnHeaders": {
-                borderBottom: "1px solid #515151 !important",
-                paddingLeft: "32px",
-              },
-              "& .MuiDataGrid-withBorderColor": {
-                border: "none",
-              },
-            }}
-          />
-          {videoRowsData.length === 0 ? <NoContent /> : null}
-        </>
-      ) : null}
-      {selected === "Shorts" ? (
-        <>
-          <DataGrid
-            rows={shortsRowsData.length !== 0 ? shortsRowsData : []}
-            columns={columns}
-            initialState={{
-              pagination: {
-                paginationModel: { page: 0, pageSize: 30 },
-              },
-            }}
-            pageSizeOptions={[30, 50, 100]}
-            checkboxSelection
-            sx={{
-              // paddingLeft: "32px",
-              borderLeft: "none",
-              borderRight: "none",
-              "& .MuiDataGrid-virtualScroller": {
-                height: shortsRowsData.length === 0 ? "55px" : "auto",
-                // paddingLeft:"32px"
-              },
-              "& .MuiDataGrid-virtualScrollerContent": {
-                height: shortsRowsData.length === 0 ? "auto" : "calc(100vh - 300px) !important",
-              },
-              "& .MuiDataGrid-cell": {
-                maxHeight: "100% !important",
-                ":focus": {
-                  outline: "none",
-                },
-              },
-              "& .MuiDataGrid-row": {
-                maxHeight: "100% !important",
-                padding: "8px 0px 8px 32px",
-                margin: "10px 0",
-                borderBottom: "1px solid #515151 !important",
-              },
-              "& .MuiDataGrid-columnHeaders": {
-                borderBottom: "1px solid #515151 !important",
-                paddingLeft: "32px",
-              },
-              "& .MuiDataGrid-withBorderColor": {
-                border: "none",
-              },
-            }}
-          />
-          {shortsRowsData.length === 0 ? <NoContent /> : null}
-        </>
-      ) : null}
+      {selected === "Videos" ? <DataTable theme={theme} columns={columns} rowsData={videoRowsData} /> : null}
+      {selected === "Shorts" ? <DataTable theme={theme} columns={columns} rowsData={shortsRowsData} /> : null}
     </div>
   );
 };
