09d9290a157913b6ff66e0dc9f44f07a7d2ad92b
Dev Code <hellodev_code@outlook.com>
2024-02-19 14:47:27 +0530

    removed theme

--- client/src/components/Page404.jsx
+++ client/src/components/Page404.jsx
@@ -2,20 +2,18 @@ import { Box, Typography } from "@mui/material";
 import React from "react";
 import Monkey404 from "../svgs/monkey.png";
 import YoutubeIcon from "../svgs/YoutubeLight.svg";
-import { useTheme } from "@emotion/react";
 
 const Page404 = () => {
-  const theme = useTheme();
   return (
     <Box sx={{ display: "flex", justifyContent: "center", alignItems: "center", flexDirection: "column", height: "100%" }}>
       <Box sx={{ marginBottom: "20px", display: "flex", alignItems: "center", flexDirection: "column" }}>
-        <img src={Monkey404} alt="" />
-        <Typography variant="h5" sx={{  marginTop: "10px" }}>
+        <img src={Monkey404} alt="error404" />
+        <Typography variant="h5" sx={{ marginTop: "10px" }}>
           This page isn't available. Sorry about that. <br />
           Try searching for something else.
         </Typography>
       </Box>
-      <img style={{ transform: "scale(1.5)" }} src={YoutubeIcon} />
+      <img style={{ transform: "scale(1.5)" }} src={YoutubeIcon} alt="youtube-icon"/>
     </Box>
   );
 };
