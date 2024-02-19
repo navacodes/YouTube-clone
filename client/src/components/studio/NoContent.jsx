c8e36733834a121e1e3a4c4fb2fc071e676e89c5
Dev Code <hellodev_code@outlook.com>
2024-02-19 18:40:14 +0530

    made css changes

--- client/src/components/studio/NoContent.jsx
+++ client/src/components/studio/NoContent.jsx
@@ -1,13 +1,28 @@
-import React from "react";
+import React, { useContext } from "react";
 import NoContentImg from "../../img/noContent.png";
 import { Box, Button, Typography } from "@mui/material";
+import { StudioContext } from "../../scenes/studio/StudioLayout";
 
 const NoContent = () => {
+  const { setUploadDialogOpen } = useContext(StudioContext);
+
   return (
     <Box className="no-content-box" sx={{ display: "flex", justifyContent: "center", alignItems: "center", flexDirection: "column" }}>
-      <img src={NoContentImg} alt="" />
+      <img src={NoContentImg} alt="No-Content" />
       <Typography sx={{ fontSize: "15px", color: "#aaa", marginBottom: "15px" }}>No content available</Typography>
-      <Button variant="contained" sx={{ color: "#0b0b0b", fontSize: "15px", fontWeight: "600", backgroundColor: "#3a99eb" }}>
+      <Button
+        onClick={() => setUploadDialogOpen(true)}
+        variant="contained"
+        sx={{
+          color: "#0b0b0b",
+          fontSize: "15px",
+          fontWeight: "600",
+          backgroundColor: "#3a99eb",
+          ":hover": {
+            backgroundColor: "#3a99eb",
+          },
+        }}
+      >
         UPLOAD VIDEOS
       </Button>
     </Box>
