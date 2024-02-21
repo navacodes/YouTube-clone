687386642efa552c54f9faa9a118db539197dfab
Dev Code <hellodev_code@outlook.com>
2024-02-21 23:57:04 +0530

    removed unsused variables

--- client/src/components/Noroute.jsx
+++ client/src/components/Noroute.jsx
@@ -2,10 +2,8 @@ import { Box, Typography } from "@mui/material";
 import React from "react";
 import Monkey404 from "../svgs/monkey.png";
 import YoutubeIcon from "../svgs/YoutubeLight.svg";
-import { useTheme } from "@emotion/react";
 
 const Noroute = () => {
-  const theme = useTheme();
   return (
     <Box sx={{ display: "flex", justifyContent: "center", alignItems: "center", flexDirection: "column", height: "100%" }}>
       <Box sx={{ marginBottom: "20px", display: "flex", alignItems: "center", flexDirection: "column" }}>
