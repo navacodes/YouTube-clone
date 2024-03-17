4a6a9003091988f3568a300a44ee5d108675d037
Dev Code <hellodev_code@outlook.com>
2024-03-17 19:09:21 +0530

    added HoverElementsModal

--- client/src/components/studio/video-card/StudioVideoCardComponent.jsx
+++ client/src/components/studio/video-card/StudioVideoCardComponent.jsx
@@ -4,50 +4,57 @@ import React, { useState } from "react";
 import HoverElements from "./HoverElements";
 import { useDispatch } from "react-redux";
 import { setVideoId } from "../../../state";
+import HoverElementsModal from "./HoverElementsModal";
 
 const StudioVideoCardComponent = ({ videoData }) => {
   const theme = useTheme();
   const [hover, setHover] = useState(false);
   const dispatch = useDispatch();
+  const [hoverModal, setHoverModal] = useState(false);
 
   return (
-    <div
-      onMouseEnter={() => {
-        setHover(true);
-        dispatch(setVideoId({ videoId: videoData.videoId }));
-      }}
-      onMouseLeave={() => setHover(false)}
-      className="studio-videocard"
-      style={{ height: "100%", width: "100%", display: "flex" }}
-    >
-      <Box className="left" sx={{ width: "120px", height: "68px" }}>
-        <video muted preload="metadata" src={videoData.mediaUrl} className="video" style={{ height: "100%" }}>
-          <source src={videoData.mediaUrl} type="video/mp4" />
-        </video>
-      </Box>
-      <Box className="right" sx={{ width: "calc(100% - 130px)", marginLeft: "16px", flex: "1 1" }}>
-        <Typography variant="h6" sx={{ fontSize: "13px", paddingTop: "8px" }}>
-          {videoData.title}
-        </Typography>
-        <Box
-          sx={{
-            fontSize: "12px",
-            whiteSpace: "wrap",
-            width: "100%",
-            height: "34px",
-            letterSpacing: "0.011em",
-            lineHeight: "16px",
-            display: "-webkit-box",
-            WebkitLineClamp: "2",
-            WebkitBoxOrient: "vertical",
-            overflow: "hidden",
-            color: theme.palette.studioLightGray,
-          }}
-        >
-          {hover ? <HoverElements theme={theme} videoId={videoData.videoId} /> : videoData.description}
+    <>
+      <div
+        onMouseEnter={() => {
+          setHover(true);
+          dispatch(setVideoId({ videoId: videoData.videoId }));
+        }}
+        onMouseLeave={() => {
+          if (!hoverModal) setHover(false);
+        }}
+        className="studio-videocard"
+        style={{ height: "100%", width: "100%", display: "flex", position: "relative" }}
+      >
+        <Box className="left" sx={{ width: "120px", height: "68px" }}>
+          <video muted preload="metadata" src={videoData.mediaUrl} className="video" style={{ height: "100%" }}>
+            <source src={videoData.mediaUrl} type="video/mp4" />
+          </video>
         </Box>
-      </Box>
-    </div>
+        <Box className="right" sx={{ width: "calc(100% - 130px)", marginLeft: "16px", flex: "1 1" }}>
+          <Typography variant="h6" sx={{ fontSize: "13px", paddingTop: "8px" }}>
+            {videoData.title}
+          </Typography>
+          <Box
+            sx={{
+              fontSize: "12px",
+              whiteSpace: "wrap",
+              width: "100%",
+              height: "34px",
+              letterSpacing: "0.011em",
+              lineHeight: "16px",
+              display: "-webkit-box",
+              WebkitLineClamp: "2",
+              WebkitBoxOrient: "vertical",
+              overflow: "hidden",
+              color: theme.palette.studioLightGray,
+            }}
+          >
+            {hover ? <HoverElements setOpen={setHoverModal} theme={theme} videoId={videoData.videoId} /> : videoData.description}
+          </Box>
+        </Box>
+      </div>
+      <HoverElementsModal cloudinaryPublicId={videoData.cloudinaryPublicId} videoId={videoData.videoId} open={hoverModal} setOpen={setHoverModal} />
+    </>
   );
 };
 
